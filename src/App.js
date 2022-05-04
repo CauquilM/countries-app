fc1e79cdfcffda6173abf14e494cc236883048d9
MathCoder <mathieufc31@gmail.com>
2022-05-04 20:37:23 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import CountriesList from "./pages/countries-list";
 
 function App() {
   return (
-    <div>
-      
+    <Router>
+      <div>
+      <nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<CountriesList/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/countries-list.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function CountriesList() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+      
+  })
+
+  return <div></div>;
+}
+
+export default CountriesList;
