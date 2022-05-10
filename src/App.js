9702970c77e48f7784d1a5d6a367d34649edfaae
MathCoder <mathieufc31@gmail.com>
2022-05-10 22:54:37 +0200

    add moon unicode

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
     nightMode ? setNightMode(false) : setNightMode(true);
   };
 
+  const unicode = "&#127769;";
+
   return (
     <Router>
       <div
@@ -47,6 +49,7 @@ function App() {
                       style={{ color: nightMode ? "white" : "black" }}
                       className="btn-flat"
                     >
+                      {nightMode ? <span style={{ fontSize: "1.5em" }}>&#127765;</span> : <span style={{ fontSize: "1.5em" }}>&#127761;</span>}
                       Dark mode
                     </a>
                   </ul>
